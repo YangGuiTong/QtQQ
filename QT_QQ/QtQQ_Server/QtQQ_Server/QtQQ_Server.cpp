ad6433a4f4e8a154aa54c2acefb51f4cc2eeb371
YangGuiTong <1763404142@qq.com>
2024-03-09 11:47:31 +0800

    1.服务端显示数据库数据

--- QT_QQ/QtQQ_Server/QtQQ_Server/QtQQ_Server.cpp
+++ QT_QQ/QtQQ_Server/QtQQ_Server/QtQQ_Server.cpp
@@ -3,6 +3,11 @@
 #include "public_type.h"
 
 #include <QPixmap>
+#include <QSqlDatabase>
+#include <QMessageBox>
+#include <QHeaderView>
+#include <QTableWidget>
+#include <QSqlRecord>
 
 QtQQ_Server::QtQQ_Server(QWidget *parent)
 	: QDialog(parent) {
@@ -14,6 +19,35 @@ QtQQ_Server::QtQQ_Server(QWidget *parent)
 	ui.label->setScaledContents(true);
 	ui.label->setPixmap(p);
 	ui.label->setAlignment(Qt::AlignCenter);
+
+	if (!connectMySql()) {
+		QString text = QString("连接数据库失败!");
+		MyLogDEBUG(text .toUtf8());
+		qDebug() << QString::fromLocal8Bit(text.toLatin1().constData());
+		QMessageBox::warning(NULL, tr("提示"), tr("连接数据库失败！"));
+		close();
+		return;
+	}
+
+	setDepNameMap();
+	setStatusMap();
+	setOnLineMap();
+
+	m_queryInfoModel = new QSqlQueryModel(this);
+	QString sql = QString("SELECT * FROM tab_employees;");
+	m_queryInfoModel->setQuery(sql);
+
+	ui.tableWidget->setEditTriggers(QAbstractItemView::NoEditTriggers);
+	updateTaleData();
+
+
+	QStringList strList;
+	strList << "人事部" << "研发部" << "市场部";
+	ui.employeeDepBox->addItems(strList);
+
+	strList.clear();
+	strList << "公司群" << "人事部" << "研发部" << "市场部";
+	ui.departmentBox->addItems(strList);
 
 
 	initTcpSocket();
@@ -35,6 +69,110 @@ void QtQQ_Server::initTcpSocket() {
 
 	// 收到tcp客户端发来的信息后进行UDP广播
 	connect(m_tcpServer, &TcpServer::signalTcpMsgComes, this, &QtQQ_Server::onUDPbroadMsg);
+}
+
+bool QtQQ_Server::connectMySql() {
+	QSqlDatabase db = QSqlDatabase::addDatabase("QMYSQL");
+	db.setDatabaseName("qtqq");
+	db.setHostName("localhost");
+	db.setUserName("root");
+	db.setPassword("yang");
+	db.setPort(3306);
+
+	if (db.open()) {
+		return true;
+	}
+
+
+	return false;
+}
+
+int QtQQ_Server::getCompDepID() {
+	return 0;
+}
+
+void QtQQ_Server::updateTaleData(int depID, int employeeID) { 
+	MyLogDEBUG(QString("更新表格数据：depID = %1, employeeID = %2").arg(depID).arg(employeeID).toUtf8());
+
+	ui.tableWidget->clear();
+
+	if (depID && depID != m_compDepID) {	// 查询部门
+		QString sql = QString("SELECT * FROM tab_employees WHERE departmentID=%1;").arg(depID);
+		MyLogDEBUG(sql.toUtf8());
+		m_queryInfoModel->setQuery(sql);
+
+	} else if (employeeID) {	// 精确查找
+		QString sql = QString("SELECT * FROM tab_employees WHERE employeeID=%1;").arg(employeeID);
+		MyLogDEBUG(sql.toUtf8());
+		m_queryInfoModel->setQuery(sql);
+	
+	} else {	// 查询所有
+		QString sql = QString("SELECT * FROM tab_employees;");
+		MyLogDEBUG(sql.toUtf8());
+		m_queryInfoModel->setQuery(sql);
+	}
+
+	int rows = m_queryInfoModel->rowCount();		// 总行数（总记录数）
+	int columns = m_queryInfoModel->columnCount();	// 总列数（总字段数）
+	QModelIndex index;	// 模型索引
+
+	// 设置表格的行数、列数
+	ui.tableWidget->setRowCount(rows);
+	ui.tableWidget->setColumnCount(columns);
+
+	// 设置表头
+	QStringList headers;
+	headers << tr("部门") << tr("工号") << tr("员工姓名") << tr("员工签名") << tr("员工状态") << tr("员工头像") << tr("在线状态");
+	ui.tableWidget->setHorizontalHeaderLabels(headers);
+
+	// 设置列等宽
+	ui.tableWidget->horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch);
+
+	for (int i = 0; i < rows; i++) {
+		for (int j = 0; j < columns; j++) {
+			index = m_queryInfoModel->index(i, j);	// 行，列
+			QString strData = m_queryInfoModel->data(index).toString();	// 获取i行j列的数据
+
+			// 获取字段名称
+			QSqlRecord record = m_queryInfoModel->record(i);	// 当前行的记录
+			QString strRecordName = record.fieldName(j);		// 当前列的记录
+
+			if (strRecordName == QLatin1String("departmentID")) {
+				ui.tableWidget->setItem(i, j, new QTableWidgetItem(m_depNameMap.value(strData)));
+				continue;
+			}
+
+			if (strRecordName == QLatin1String("status")) {
+				ui.tableWidget->setItem(i, j, new QTableWidgetItem(m_statusMap.value(strData)));
+				continue;
+			}
+
+			if (strRecordName == QLatin1String("online")) {
+				ui.tableWidget->setItem(i, j, new QTableWidgetItem(m_onlineMap.value(strData)));
+				continue;
+			}
+
+			ui.tableWidget->setItem(i, j, new QTableWidgetItem(strData));
+		}
+	}
+}
+
+void QtQQ_Server::setDepNameMap() {
+	m_depNameMap.insert("2001", tr("人事部"));
+	m_depNameMap.insert("2002", tr("研发部"));
+	m_depNameMap.insert("2003", tr("市场部"));
+}
+
+void QtQQ_Server::setStatusMap() {
+	m_statusMap.insert("1", tr("有效"));
+	m_statusMap.insert("2", tr("已注销"));
+}
+
+void QtQQ_Server::setOnLineMap() {
+	m_onlineMap.insert("1", tr("离线"));
+	m_onlineMap.insert("2", tr("在线"));
+	m_onlineMap.insert("3", tr("隐身"));
+	m_onlineMap.insert("4", tr("忙碌"));
 }
 
 
--- QT_QQ/QtQQ_Server/QtQQ_Server/QtQQ_Server.h
+++ QT_QQ/QtQQ_Server/QtQQ_Server/QtQQ_Server.h
@@ -5,6 +5,8 @@
 
 #include "TcpServer.h"
 
+#include <QSqlQueryModel>
+
 class QtQQ_Server : public QDialog
 {
     Q_OBJECT
@@ -15,7 +17,13 @@ public:
 
 private:
 	void initTcpSocket();
+	bool connectMySql();
+	int getCompDepID();	// 获取公司群QQ号
+	void updateTaleData(int depID = 0, int employeeID = 0);
 
+	void setDepNameMap();
+	void setStatusMap();
+	void setOnLineMap();
 
 private slots:
 	void onUDPbroadMsg(QByteArray &btData);
@@ -24,4 +32,11 @@ private:
     Ui::QtQQ_ServerClass ui;
 
 	TcpServer *m_tcpServer;
+
+	int m_compDepID;		// 公司群QQ号
+	QSqlQueryModel *m_queryInfoModel;	// 查询所有员工信息模型
+
+	QMap<QString, QString> m_statusMap;		// 状态
+	QMap<QString, QString> m_depNameMap;	// 部门名称
+	QMap<QString, QString> m_onlineMap;		// 在线状态
 };
